80a0551b915a63a1e1a8628ba58d8f3ba02e8d18
Jianmin Zhou <zhoujianmin@xiaoyezi.com>
2018-01-13 19:23:35 +0800

    Add "start" page
    Change-Id: Ieabf83ef4eb5eef599a7660e79d92f7db11378de

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+    wx.setStorageSync('appName', "爱宝思维");
+
     // 登录
     wx.login({
       success: res => {
@@ -32,6 +34,24 @@ App({
         }
       }
     })
+  },
+
+  getUserInfo: function(cb) {
+    var that = this;
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo);
+    } else {
+      wx.login({
+        success: function() {
+          wx.getUserInfo({
+            success: function(res) {
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo);
+            }
+          });
+        }
+      });
+    }
   },
   globalData: {
     userInfo: null
